dc7da52d711964c8c2cbfc17d9a0f2b108e3daa3
lironshani <liron05481@gmail.com>
2021-12-29 23:03:36 +0200

    Adding db host to env

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const client = new Client({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  host: "localhost",
+  host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   database: process.env.POSTGRES_DATABASE,
 });
