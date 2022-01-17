5ec823bdeb95b1a76136618d21a40fec97692b21
lironshani <liron05481@gmail.com>
2022-01-17 15:36:59 +0200

    changing the email content, cleaning code

--- email.js
+++ email.js
@@ -11,32 +11,18 @@ var transporter = nodemailer.createTransport({
 const forgotPasswordMailOptions = (email, token) => ({
   from: "Cars Treatments",
   to: email,
-  subject: "Reset Password",
+  subject: "Reset Password- Car Spa",
   html: `
     <h2>Click on link to reset your password</h2>
     <a href="${process.env.CLIENT_URL}/reset-password/${token}">Click here to reset</a>
+    <p> Car Spa</p>
   `,
 });
 const signUpMailOptions = (email) => ({
   from: "Cars Treatments",
   to: email,
-  subject: "Thank you for signing up to Cars Treatments",
-  text: "You have successfully signed up to Cars Treatments",
-  //   html:
-  //     "<h4><b>Activate Account</b></h4>" +
-  //     "<p>To activate your account, please enter this URL:</p>" +
-  //     "<a href=https://techstar12.herokuapp.com/activate/" +
-  //     user.id +
-  //     "/" +
-  //     token +
-  //     ">" +
-  //     "https://techstar12.herokuapp.com/activate/" +
-  //     user.id +
-  //     "/" +
-  //     token +
-  //     "</a>" +
-  //     "<br><br>" +
-  //     "<p>--Team</p>",
+  subject: "Thank you for signing up to Car Spa",
+  text: "You have successfully signed up to Cat Spa",
 });
 
 const mailOptions = (type, args) => {
